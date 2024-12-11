e0ab68e26a46f696f8bfb897f2f6101e56fe8df2
bhanuprakash <bhanuprakashande9@gmail.com>
2024-12-11 11:40:10 +0530

    api my link commit

--- src/suby/api.js
+++ src/suby/api.js
@@ -1,5 +1,5 @@
-export const API_URL = "https://backend-nodejs-suby.onrender.com"
+// export const API_URL = "https://backend-nodejs-suby.onrender.com"
 
 // export const API_URL = "http://localhost:4000"
 
-// export const API_URL = "https://backendswiggy2.onrender.com"
+export const API_URL = "https://backendswiggy2.onrender.com"
